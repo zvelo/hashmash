beb58c4dda7d49c3e483c84bdc285af6107b783c
Joshua Rubin <jrubin@zvelo.com>
2013-06-17 16:06:50 -0600

    ensure polyfills are loaded first
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- example/public/js/main.js
+++ example/public/js/main.js
@@ -1,12 +1,21 @@
 (function() {
-  requirejs.config({
+  require.config({
     baseUrl: "js",
     paths: {
       jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min"
+    },
+    packages: [
+      {
+        name: "poly",
+        location: "poly",
+        main: "poly"
+      }
+    ],
+    deps: ["poly/function", "jquery"],
+    callback: function() {
+      return require(["example"]);
     }
   });
-
-  require(["poly/function", "jquery", "example"]);
 
 }).call(this);
 
--- example/src/main.coffee
+++ example/src/main.coffee
@@ -1,6 +1,11 @@
-requirejs.config
+require.config
   baseUrl: "js"
   paths:
     jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min"
-
-require [ "poly/function", "jquery", "example" ]
+  packages: [
+    name:     "poly"
+    location: "poly"
+    main:     "poly"
+  ]
+  deps: [ "poly/function", "jquery" ]
+  callback: -> require [ "example" ]
